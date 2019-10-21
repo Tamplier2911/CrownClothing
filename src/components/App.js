7b4ddd7138fe770c178ebb32d69f5cacfd74756a
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2019-10-22 00:22:16 +0300

    Set up storing users information from Google Auth into firestore.

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,10 @@ import HomePage from "../pages/HomePage/HomePage";
 import ShopPage from "../pages/ShopPage/ShopPage";
 import SignInPage from "../pages/SignInPage/SignInPage";
 
-import { auth } from "../firebase/firebase.utils";
+import {
+  auth,
+  createUserProfileDocument
+} from "../firebase/firebase.utils";
 
 class App extends Component {
   constructor(props) {
@@ -23,9 +26,24 @@ class App extends Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(
-      user => {
-        this.setState({ currentUser: user });
-        console.log(this.state.currentUser);
+      async userAuth => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(
+            userAuth
+          );
+          userRef.onSnapshot(snapShot =>
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            })
+          );
+        } else {
+          this.setState({ currentUser: userAuth });
+        }
+        // this.setState({ currentUser: userAuth });
+        console.log(userAuth);
       }
     );
   }

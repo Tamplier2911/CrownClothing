27480011c98b8c0a7f206bc5c4dd4d779d570440
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2019-12-19 13:12:19 +0200

    Rebuilding with fixed bugs.

---
+++ client/src/environment.js
@@ -0,0 +1,16 @@
+// Stripe Public Key
+export const stripeApiKey = "pk_test_OLIJwk3gtcJJuefeWBtwLYt900meypndno";
+// export const stripeApiKey = "pk_test_KaRPQReBBNZ8yMVXgK6loII400IX5k1kCa";
+// pk_test_OLIJwk3gtcJJuefeWBtwLYt900meypndno - for new stripe account CrwnCloth
+
+// Firebase Public Key and other config.
+export const config = {
+  apiKey: "AIzaSyDwyY2zeWSXh3NvUpVL2HyjBW-o7gu2jcY",
+  authDomain: "crown-clothing-db-1fa25.firebaseapp.com",
+  databaseURL: "https://crown-clothing-db-1fa25.firebaseio.com",
+  projectId: "crown-clothing-db-1fa25",
+  storageBucket: "crown-clothing-db-1fa25.appspot.com",
+  messagingSenderId: "585679593103",
+  appId: "1:585679593103:web:7c15c9929318d748c39afa",
+  measurementId: "G-GQF0YKFH61"
+};

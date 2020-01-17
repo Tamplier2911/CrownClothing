c2ed4868f563e89ac2a0b3532f986a2c684f7ef4
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2020-01-17 14:32:57 +0200

    Uploading latest build to Heroku, wrapping up project.

--- client/src/contexts/CollectionsContext/collection-context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import SHOP_DATA from "./collections";
-
-const CollectionContext = createContext(SHOP_DATA);
-
-export default CollectionContext;
--- client/src/contexts/UserContext/current-user.js
+++
@@ -1,8 +0,0 @@
-// current user state example
-
-const CURRENT_USER = {
-  createAt: (Timestamp = { seconds: 1571684921, nanoseconds: 768000000 }),
-  displayName: "Artem Nikolaiev",
-  email: "artem.nikolaiev@gmail.com",
-  id: "02IrFf4cL2OrXEeZxvjM6tquP431"
-};
